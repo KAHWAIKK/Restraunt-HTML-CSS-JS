2a3e0bb41dcdcd352163b3cfe0fa3eedbed91e96
KAHWAIKK <kahwaikagwima@gmail.com>
2025-05-14 23:53:31 +0300

    added hero section mark-up ,styled it adding animations and added functionality on the slider using JS

--- js/main.js
+++ js/main.js
@@ -85,3 +85,53 @@ window.addEventListener("scroll" , function () {
 header.classList.add("active"): Adds the class active to the header (usually to apply a different style).
 
 else: Removes it again when back to the top. */
+
+/* == HERO SLIDER=== */
+
+const heroSlider = document.querySelector("[data-hero-slider]")
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]")
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]")
+const heroSliderNextBtn = document.querySelector("[data-prev-btn]")
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0]
+
+const updateSliderPos = function () {
+    lastActiveSliderItem.classList.remove("active")
+    heroSliderItems[currentSlidePos].classList.add("active")
+    lastActiveSliderItem=heroSliderItems[currentSlidePos]
+}
+
+const slideNext = function () {
+    if(currentSlidePos >= heroSliderItems.length -1){
+        currentSlidePos = 0
+    } else {
+        currentSlidePos++;
+    }
+
+    updateSliderPos()
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext)
+
+const slidePrev = function () {
+    if (currentSlidePos <= 0){
+        heroSliderItems.length - 1
+    }else {
+        currentSlidePos--;
+    }
+
+    updateSliderPos()
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev)
+
+/* == AUTO-SLIDE */
+
+let autoSlideInterval;
+
+const autoSlide = function() {
+    autoSlideInterval = setInterval(function () {
+        slideNext();
+    },7000)
+}
